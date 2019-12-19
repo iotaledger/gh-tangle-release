c25980940f15d2b37b349e2405743bffcc6f59cc
Martyn Janes <martyn@obany.com>
2019-12-19 13:12:15 +0100

    Additional logging

--- dist/index.js
+++ dist/index.js
@@ -10369,7 +10369,18 @@ async function run() {
       tag: tagName.replace('refs/tags/', '')
     });
 
-    console.log(release);
+    // eslint-disable-next-line camelcase
+    const { url, assets_url, upload_url, html_url, name, body, tarball_url, zipball_url, assets } = release.data;
+
+    console.log('url', url);
+    console.log('assets_url', assets_url);
+    console.log('upload_url', upload_url);
+    console.log('html_url', html_url);
+    console.log('name', name);
+    console.log('body', body);
+    console.log('tarball_url', tarball_url);
+    console.log('zipball_url', zipball_url);
+    console.log('assets', assets);
   } catch (error) {
     core.setFailed(error.message);
   }
